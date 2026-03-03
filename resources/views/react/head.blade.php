3ea5f47ff0ceef0c0a9e74d99b2fa2c3d7908670
dario.mata <dario.mata@ip-192-168-1-72.ec2.internal>
2026-03-03 16:24:01 -0600

    feat: refine payment types display, translate to spanish and filter relevant options

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-DqzpGGMH.css" />
-<script type="module" crossorigin src="/react/index-CM3RnXIQ.js"></script>
+<script type="module" crossorigin src="/react/index-BWMS7b4B.js"></script>
