7b625f0486f6c12ca53cd57f9b80f836f3d111da
dario.mata <dario.mata@ip-192-168-1-72.ec2.internal>
2026-03-03 18:18:01 -0600

    feat: display subtotal and total breakdown in Expenses

---
+++ public/react/useDownloadPdfs-DSPbP1fk.js
@@ -0,0 +1,9 @@
+import{ab as t,r as o,e as u}from"./index-XWPutRMp.js";import{u as a}from"./react-query-Ci6zupQw.js";/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */function l({entity:e}){const n=a();return s=>{s.length&&(t.processing(),n.fetchQuery([`/api/v1/${e}s/bulk`],()=>o("POST",u(`/api/v1/${e}s/bulk`),{action:"bulk_download",ids:s}).then(()=>t.success("downloaded_entities"))))}}export{l as u};
--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-BOrqXdUe.css" />
-<script type="module" crossorigin src="/react/index-CUxu7x-s.js"></script>
+<script type="module" crossorigin src="/react/index-XWPutRMp.js"></script>
