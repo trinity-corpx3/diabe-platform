4947faf239cec7a20fbf185909d033213360c2a2
dario.mata <dario.mata@ip-192-168-1-72.ec2.internal>
2026-03-03 18:56:48 -0600

    chore: update build hashes

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="/react/index-BOrqXdUe.css" />
-<script type="module" crossorigin src="/react/index-XWPutRMp.js"></script>
+<link rel="stylesheet" href="/react/index-Bb376nTQ.css" />
+<script type="module" crossorigin src="/react/index-D4oQY6U-.js"></script>
