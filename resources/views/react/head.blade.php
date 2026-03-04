5598de83494023145e0979e24e90ba97c15238c0
dario.mata <dario.mata@ip-192-168-1-72.ec2.internal>
2026-03-03 18:08:03 -0600

    fix: correct payroll date range logic using isoWeek

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-BOrqXdUe.css" />
-<script type="module" crossorigin src="/react/index-D4rNGnBS.js"></script>
+<script type="module" crossorigin src="/react/index-CUxu7x-s.js"></script>
