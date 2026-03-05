8a38d1baec9332d6de88a34060a05d2e06027dca
dario.mata <dario.mata@D2L6MFPL71.local>
2026-03-05 15:14:44 -0600

    feat: hide support icons (contact, forum, guide, about)

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-Bb376nTQ.css" />
-<script type="module" crossorigin src="/react/index-LFuums8v.js"></script>
+<script type="module" crossorigin src="/react/index-JHL-Vbll.js"></script>
