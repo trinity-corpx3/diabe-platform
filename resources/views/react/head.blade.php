1f5d868480ea51c37818451c78361a78adf86051
dario.mata <dario.mata@192.168.1.70>
2026-03-17 17:21:30 -0600

    feat: deploy new React frontend build with NaN fix in OfficeExpense

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-Bb376nTQ.css" />
-<script type="module" crossorigin src="/react/index-Bghybimd.js"></script>
+<script type="module" crossorigin src="/react/index-C7FyyrAX.js"></script>
