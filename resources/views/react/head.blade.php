6ab17d135405ad0be0c0111fc528938fd726b2d6
dario.mata <dario.mata@192.168.1.70>
2026-03-17 20:16:53 -0600

    fix: load latest react office expenses bundle

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-Bb376nTQ.css" />
-<script type="module" crossorigin src="/react/index-DahyfNl9.js"></script>
+<script type="module" crossorigin src="/react/index-BNz6LfrH.js"></script>
