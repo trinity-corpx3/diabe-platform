c29b12eb597210f85c5dee7cac9fe91a319ab473
dario.mata <dario.mata@192.168.1.70>
2026-03-17 18:25:45 -0600

    fix(office-expenses): fix 422 error and duplicate buttons in row actions

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-Bb376nTQ.css" />
-<script type="module" crossorigin src="/react/index-C7FyyrAX.js"></script>
+<script type="module" crossorigin src="/react/index-DahyfNl9.js"></script>
