52b12b4c130d4923681d896506dd253e71754ca0
dario.mata <dario.mata@192.168.1.70>
2026-03-17 20:46:14 -0600

    fix: update office expense create label

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-Bb376nTQ.css" />
-<script type="module" crossorigin src="/react/index-6e_Z-lX0.js"></script>
+<script type="module" crossorigin src="/react/index-C59dQZQc.js"></script>
