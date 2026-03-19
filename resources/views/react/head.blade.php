02eaed687a6590d64f2b66622bcd6636402e5684
dario.mata <dario.mata@D2L6MFPL71.local>
2026-03-19 15:06:42 -0600

    fix: point react entry to current build hash

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-Bb376nTQ.css" />
-<script type="module" crossorigin src="/react/index-Cwi2cZBv.js"></script>
+<script type="module" crossorigin src="/react/index-Df7vv8CZ.js"></script>
