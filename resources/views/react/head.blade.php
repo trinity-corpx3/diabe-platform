330896f2ee3773c2f0001c1337eae6513c4415b9
dario.mata <dario.mata@192.168.1.70>
2026-03-20 14:46:32 -0600

    fix registros 7

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-CIHAOVJv.css" />
-<script type="module" crossorigin src="/react/index-4ReMflv5.js"></script>
+<script type="module" crossorigin src="/react/index-DdzKED_7.js"></script>
