a94ce58b4c6b28d51af2afb4b6893f90a82271cb
dario.mata <dario.mata@192.168.1.70>
2026-03-20 14:38:23 -0600

    fix registros 6

--- public/react/queries-CnwIlhpB.js
+++
@@ -1,9 +0,0 @@
-import{E as n,r,e as t}from"./index-CHSZFnfy.js";import{a as i}from"./react-query-Ci6zupQw.js";/**
- * Invoice Ninja (https://invoiceninja.com).
- *
- * @link https://github.com/invoiceninja/invoiceninja source repository
- *
- * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
- *
- * @license https://www.elastic.co/licensing/elastic-license
- */function c(e){const a=n();return i(["/api/v1/credits","create"],()=>r("GET",t("/api/v1/credits/create")).then(s=>s.data.data),{...e,staleTime:1/0,enabled:a("create_credit")?(e==null?void 0:e.enabled)??!0:!1})}function l({id:e}){return i(["/api/v1/credits",e],()=>r("GET",t("/api/v1/credits/:id?include=client",{id:e})).then(a=>a.data.data),{staleTime:1/0})}export{l as a,c as u};
--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-CIHAOVJv.css" />
-<script type="module" crossorigin src="/react/index-CHSZFnfy.js"></script>
+<script type="module" crossorigin src="/react/index-4ReMflv5.js"></script>
