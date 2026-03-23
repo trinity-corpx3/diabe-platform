873762b4049d907f0a0ead478b0902c0b7b10d1b
dario.mata <dario.mata@D2L6MFPL71.local>
2026-03-23 17:31:20 -0600

    fix: Add type='button' to month navigation buttons
    - Prevents page reload when clicking navigation buttons
    - Buttons now properly change month state without form submission
    - New JS hash: index-DH0LXY3u.js

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-BF_k-BQi.css" />
-<script type="module" crossorigin src="/react/index-DiPAZxKh.js"></script>
+<script type="module" crossorigin src="/react/index-DH0LXY3u.js"></script>
