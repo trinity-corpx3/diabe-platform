3fc65f6761bb0411c38183e782f5f96e5bf699c8
dario.mata <dario.mata@D2L6MFPL71.local>
2026-03-23 17:49:34 -0600

    feat: Add Spanish translation for 'view_all_months' in React
    - Add 'view_all_months': 'Ver todos los meses' to es.json and es_ES.json
    - Link now displays in Spanish instead of English
    - New JS hash: index-Diwk5nP-.js

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-C3AWLebS.css" />
-<script type="module" crossorigin src="/react/index-Ch69ItYf.js"></script>
+<script type="module" crossorigin src="/react/index-Diwk5nP-.js"></script>
