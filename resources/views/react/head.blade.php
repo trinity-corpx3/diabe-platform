5e7b15473f6eabe963f502e8e04834b0b99d6e40
dario.mata <dario.mata@D2L6MFPL71.local>
2026-03-23 17:27:11 -0600

    feat: Add month navigation to PendingPayments card
    - Add previous/next month buttons with arrow icons
    - Add 'current month' button when viewing past/future months
    - Update currentMonth state when navigating
    - React Query automatically refetches data when month changes
    - New hashes: index-BF_k-BQi.css, index-DiPAZxKh.js

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="/react/index-B3Xk2lYC.css" />
-<script type="module" crossorigin src="/react/index-H8Xz5BP1.js"></script>
+<link rel="stylesheet" href="/react/index-BF_k-BQi.css" />
+<script type="module" crossorigin src="/react/index-DiPAZxKh.js"></script>
