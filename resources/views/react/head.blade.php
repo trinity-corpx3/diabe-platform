40f52995b302d6f999a9bd5ce69bf453f577a645
dario.mata <dario.mata@D2L6MFPL71.local>
2026-03-23 16:38:28 -0600

    fix: Move React build files to correct location
    - Move files from public/react/react/ to public/react/
    - Update head.blade.php to use correct paths
    - Fix 404 errors on React module imports

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="/react/react/index-B3Xk2lYC.css" />
-<script type="module" crossorigin src="/react/react/index-BJ_9OEzh.js"></script>
+<link rel="stylesheet" href="/react/index-B3Xk2lYC.css" />
+<script type="module" crossorigin src="/react/index-BJ_9OEzh.js"></script>
