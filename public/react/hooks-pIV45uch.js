6cc15e54a21c27d0144c1dd12d4aa9cbd7bce27b
dario.mata <dario.mata@D2L6MFPL71.local>
2026-03-23 17:18:36 -0600

    fix: Remove endpoint() helper to prevent URL duplication
    - Use direct relative paths instead of endpoint() wrapper
    - Change DataTable endpoint prop to use route() instead
    - New JS hash: index-DAAiDmlM.js
    - This should fix the URL duplication by avoiding apiEndpoint() concatenation

---
+++ public/react/hooks-pIV45uch.js
@@ -0,0 +1,9 @@
+import{eI as s}from"./index-DAAiDmlM.js";import{a as n}from"./react-redux-aYPvepl6.js";/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */function r(){const t=n();return(e,a)=>{t(s({object:"company",property:`settings.${e}`,value:a}))}}export{r as u};
--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-B3Xk2lYC.css" />
-<script type="module" crossorigin src="/react/index-C-wJY2ux.js"></script>
+<script type="module" crossorigin src="/react/index-DAAiDmlM.js"></script>
