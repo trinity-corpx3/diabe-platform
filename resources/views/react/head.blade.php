c56afcbda2627cb83fe7919ef5b893766adab2e4
dario.mata <dario.mata@D2L6MFPL71.local>
2026-03-23 23:20:48 -0600

    fix: Disable invoice and client links in PendingPayments card
    - Remove DynamicLink from invoice_number and client_name columns
    - Links were causing 404/400 errors because referenced invoices don't exist
    - Display invoice numbers and client names as plain text instead
    - Prevents 'Record with value X not found' errors
    - New JS hash: index-DpGQZ47K.js
    
    Note: Backend needs to fix data integrity in pending payments table

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-nmmJKyh5.css" />
-<script type="module" crossorigin src="/react/index-BJp56uZv.js"></script>
+<script type="module" crossorigin src="/react/index-DpGQZ47K.js"></script>
