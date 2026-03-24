3bc616c308a2b6b8b7bae2917eaf8f763200c1cb
dario.mata <dario.mata@D2L6MFPL71.local>
2026-03-23 23:10:05 -0600

    fix: Remove is_locked parameter completely from invoice query
    - Remove is_locked=true parameter that was causing 400 Bad Request
    - Endpoint now uses only: /api/v1/invoices/:id?include=payments,client.group_settings
    - Both show_schedule and is_locked parameters removed
    - Fixes persistent 400 error when clicking invoice links
    - New JS hash: index-BJp56uZv.js

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-nmmJKyh5.css" />
-<script type="module" crossorigin src="/react/index-B4hRlM6O.js"></script>
+<script type="module" crossorigin src="/react/index-BJp56uZv.js"></script>
