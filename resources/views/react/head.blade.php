d1302d32eaf914b6c0fdb387ef04f7c1c81a5d0e
dario.mata <dario.mata@D2L6MFPL71.local>
2026-03-24 18:05:07 -0600

    feat: Display net pay with discounts in payroll table
    Frontend changes:
    - Added total_discounts and net_pay to WorkerEntry interface
    - Modified Total column to show net_pay instead of total_pay
    - Shows discount amount in red below net pay when applicable
    - Format: Net amount with '- desc.' indicator
    
    Backend changes:
    - PayrollController now calculates and returns total_discounts and net_pay
    - Queries discountApplications relationship for each entry
    - net_pay = total_pay - total_discounts
    
    Result: When a discount is added after payroll creation, the table
    automatically shows the reduced net amount (e.g., ,000 -  = ,500)
    
    New hash: JS index-yHVDMQFx.js

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-CtJMg3ix.css" />
-<script type="module" crossorigin src="/react/index-QQd2C-NS.js"></script>
+<script type="module" crossorigin src="/react/index-yHVDMQFx.js"></script>
