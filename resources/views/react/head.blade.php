5ace19969c15862eb0f50c72717ef6923cbb43ba
dario.mata <dario.mata@D2L6MFPL71.local>
2026-03-24 18:11:23 -0600

    feat: Add dedicated Discounts column to payroll table
    - Added 'Descuentos' column between 'Pago Extra' and 'Neto'
    - Changed 'Total' header to 'Neto' for clarity
    - Discounts show in red with minus sign: -
    - Shows '—' when no discounts applied
    - Makes it crystal clear which employees have discounts
    
    Layout now:
    | Pago Extra | Descuentos | Neto |
    |        | -      | ,500 |
    
    New hash: JS index--7_RjHJB.js

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-CtJMg3ix.css" />
-<script type="module" crossorigin src="/react/index-yHVDMQFx.js"></script>
+<script type="module" crossorigin src="/react/index--7_RjHJB.js"></script>
