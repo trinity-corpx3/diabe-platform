6d36ad487d0f7ec874a499252a06bb222b87e123
dario.mata <dario.mata@D2L6MFPL71.local>
2026-03-24 22:37:15 -0600

    fix: Correct asset paths in head.blade.php
    - Moved files from /react/react/ to /react/
    - Updated paths to /react/index-CtJMg3ix.css and /react/index-2cfoSEyn.js
    - This fixes all 404 errors for missing JS/CSS assets

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="/react/react/index-CtJMg3ix.css" />
-<script type="module" crossorigin src="/react/react/index-2cfoSEyn.js"></script>
+<link rel="stylesheet" href="/react/index-CtJMg3ix.css" />
+<script type="module" crossorigin src="/react/index-2cfoSEyn.js"></script>
