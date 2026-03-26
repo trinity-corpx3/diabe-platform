eeb94e77d483cb2fba7a74722c6ae67aab130255
dario.mata <dario.mata@192.168.1.86>
2026-03-25 18:12:12 -0600

    fix nomina

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -1,2 +1,2 @@
 <link rel="stylesheet" href="/react/index-CtJMg3ix.css" />
-<script type="module" crossorigin src="/react/index-U_SOzWFx.js"></script>
+<script type="module" crossorigin src="/react/index-BBylswxI.js"></script>
